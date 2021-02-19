6187211caa29298471861a60cd5ad965d94008d9
tcldl <1503991273@qq.com>
2021-02-19 10:47:42 +0800

    test idea commit

--- src/main/java/com/ldl/controller/HellowController.java
+++ src/main/java/com/ldl/controller/HellowController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller
 @RestController
 public class HellowController {
-    //请求web页面
+    //请求web页面,直接运行
     @RequestMapping("/hellow")
     public String hellowSpringBoot(){
         return "hellow";
